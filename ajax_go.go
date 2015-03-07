c04730a7e8a33d67022e778ed9f728702bc325a2
utkir <usoatov@gmail.com>
2015-03-07 13:21:00 +0500

    load html from file

--- ajax_go.go
+++ ajax_go.go
@@ -7,90 +7,12 @@ import (
 	"net/http"
 )
 
-// Content for the main html page..
-var page = `<html>
-           <head>
-             <script type="text/javascript"
-               src="http://localhost:9999/jquery.min.js">
-             </script>
-             <style> 
-               div {
-                 font-family: "Times New Roman", Georgia, Serif;
-                 font-size: 1em;
-                 width: 13.3em;
-                 padding: 8px 8px; 
-                 border: 2px solid #2B1B17;
-                 color: #2B1B17;
-                 text-shadow: 1px 1px #E5E4E2;
-                 background: #FFFFFF;
-               }
-             </style>
-           </head>
-           <body>
-             <h2 align=center>Go Timer </h2>
-             <div id="output" style="width: 30%; height: 64%; overflow-y: scroll; float:left;"></div>
-             <div id="v1" style="width: 50%; height: 30%; overflow-y: scroll; float:left;"></div>
-             <div id="v2" style="width: 50%; height: 30%; overflow-y: scroll; float:left;"></div>
-             <table width=100%>
-             <tr>
-             <td>
-             <p></p>
-             <input id="sett" type="submit" name="sett" value="Settings" onclick="changeUrl()">            
-             </td>
-             </tr>
-
-             </table>
-
-             <script type="text/javascript">
-
-               var myDelay;
-
-               $(document).ready(function () 
-               {
-                   $("#output").append("Waiting for system time..");
-                   
-                   myDelay = setInterval("delayedPost()", 1000);               
-
-                });
-
-               function delayedPost() 
-               {
-                 $.post("http://localhost:9999/dev", "", function(data, status) 
-                 {
-                    //$("#output").empty();
-                    $("#output").prepend(data);
-                 });
-
-                 $.post("http://localhost:9999/v1", "", function(data, status) {
-                    //$("#output").empty();
-                    $("#v1").prepend(data);
-                 });
-
-                 $.post("http://localhost:9999/v2", "", function(data, status) {
-                    //$("#output").empty();
-                    $("#v2").prepend(data);
-                 });
-               }
-
-
-               function changeUrl()
-               {
-                  //alert('salom');                      
-                  clearInterval(myDelay);
-
-                  window.location.replace("http://localhost:9999/sett");
-
-               }
-
-
-
-             </script>
-           </body>
-         </html>`
-
 // handler for the main page.
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, page)
+	// Content for the main html page..
+
+	page, _ := ioutil.ReadFile("html/index.html")
+	fmt.Fprint(w, string(page))
 }
 
 // handler to cater AJAX requests
